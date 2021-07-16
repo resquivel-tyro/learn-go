bc32eee66db43046c57758392ed39e4d5cb45563
Rhoda Esquivel <resquivel@tyro.com>
2021-07-16 16:50:46 +1000

    Withdraw fcn

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -21,16 +21,20 @@ type Stringer interface {
 //"a pointer to a wallet".
 
 
-func (w *Wallet) Deposit(i Bitcoin) {
-	w.balance += i
+func (w *Wallet) Deposit(b Bitcoin) {
+	w.balance += b
 	// the language permits us to write w.balance, without an explicit dereference like (*w).balance
 	// these are struct pointers and are automatically dereferenced
 }
 
-func (w *Wallet) Balance() (i Bitcoin) {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 	//	Technically you do not need to change Balance to use a pointer receiver as taking a copy of the balance is fine.
 	//	However, by convention you should keep your method receiver types the same for consistency.
+}
+
+func (w *Wallet) Withdraw(b Bitcoin) {
+	w.balance-=b
 }
 
 func (b Bitcoin) String() string {
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,15 +3,33 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet {}
-
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	assertBalance := func (t *testing.T, got Bitcoin, want Bitcoin) {
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet {}
+
+		wallet.Deposit(10)
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		assertBalance(t, got, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet {balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		assertBalance(t, got, want)
+	})
 }
