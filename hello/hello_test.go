12751456878705ca1be794bae96e8110b18f90cc
Rhoda Esquivel <resquivel@tyro.com>
2021-07-16 11:59:23 +1000

    Default to printing 'Hello, World' given an empty string

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 
+// Basic Test Hello
 func TestHello(t *testing.T) {
 	got := Hello("Bob")
 	want := "Hello, Bob"
@@ -9,4 +10,29 @@ func TestHello(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestHelloUsingSubtests(t *testing.T) {
+	// testing.TB is an interface that both *testing.T and *testing.B satisfy
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()	// needed to tell the test suite that this method is a helper
+		// By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Bob")
+		want := "Hello, Bob"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
