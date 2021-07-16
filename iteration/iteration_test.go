5b9f8c5d4e129af7aec44f05ed3d9110e7298b8d
Rhoda Esquivel <resquivel@tyro.com>
2021-07-16 12:45:28 +1000

    Benchmark iteration function

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -10,3 +10,19 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+// when benchmark code is executed, it runs b.N times and measures how long it takes
+// the framework will determine what is a "good" value to get decent results
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+// Sample benchmark output in console:
+//goos: darwin
+//goarch: amd64
+//pkg: hello/iteration
+//cpu: Intel(R) Core(TM) i7-8750H CPU @ 2.20GHz
+//BenchmarkRepeat
+//BenchmarkRepeat-12    	 9531441	       118.0 ns/op
+//PASS
